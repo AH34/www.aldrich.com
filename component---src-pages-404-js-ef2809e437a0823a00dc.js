40f15a953695d74f33620144db7b48d87f3b52d7
Calvert <yz3210@gmail.com>
2020-04-23 09:51:19 +0800

    Site updated: 2020-04-23 09:51:16

--- component---src-pages-404-js-ef2809e437a0823a00dc.js
+++ component---src-pages-404-js-ef2809e437a0823a00dc.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("Wbzz");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")})),null}}}]);
-//# sourceMappingURL=component---src-pages-404-js-b79fd7d172198fd2c7e5.js.map
+//# sourceMappingURL=component---src-pages-404-js-ef2809e437a0823a00dc.js.map
